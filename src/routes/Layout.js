6288f30627d62803bfa63fad7c4424a16859a32d
David Sorensen <teardownthismall@gmail.com>
2022-09-17 08:18:49 -0400

    added container and border

--- src/routes/Layout.js
+++ src/routes/Layout.js
@@ -1,8 +1,10 @@
+import { Container } from "@mui/material";
 import React, { useEffect } from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import { primary } from "../themes/primary";
 
 const sections = [
     {
@@ -77,12 +79,20 @@ const Layout = () => {
     }, [location]);
 
     return (
-        <>
+        <Container
+            disableGutters
+            maxWidth="xl"
+            sx={{
+                borderLeft: primary.border,
+                borderRight: primary.border,
+                borderBottom: primary.border,
+            }}
+        >
             {location.pathname === "/" && <Hero />}
             <Header />
             <Outlet />
             <Footer sections={sections} />
-        </>
+        </Container>
     );
 };
 
